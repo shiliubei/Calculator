8c1f2f5fd011b37713309eaf00ba37ec3e55fb43
shiliubei <shiliubei@gmail.com>
2019-06-27 00:56:58 +0900

    Test equals added

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,18 +4,18 @@ import calculator.Exp;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
+    Calculator calc = new Calculator();
 
     /**
      * Test calculator.
      */
     @Test
     public void whenParse() {
-        Calculator calc = new Calculator();
-        calc.parse("1 + 10");
         Exp exp = new Exp("1", "1", "+", "arabic");
-        assertThat(calc.parse("1 + 1"), is(exp));
+        assertEquals(this.calc.parse("1 + 1"), exp);
     }
 }
